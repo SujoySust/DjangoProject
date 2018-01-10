903e877c5d23343a587f520f7384beddd7ed2500
SujoySust <sujoy.cse50@gmail.com>
2018-01-10 10:20:46 +0600

    templating

--- mysite/personal/urls.py
+++ mysite/personal/urls.py
@@ -2,4 +2,7 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post', views.addpost, name='addpost'),
+    path('postlist',views.postlist, name='postlist'),
+    path('inbox',views.inbox, name='inbox'),
 ]
--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'personal/home.html')
+def addpost(request):
+    return  render(request,'personal/post/addpost.html')
+def postlist(request):
+    return render(request,'personal/post/postlist.html')
+def inbox (request):
+    return render(request,'personal/inbox/inbox.html')
